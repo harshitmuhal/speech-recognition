8ead1f28c883cb4f99c68e5b13bebc03721f699d
harshit muhal <hrshtmuhal8@gmail.com>
2019-07-18 11:01:44 +0530

    added files

---
+++ README.md
@@ -0,0 +1,12 @@
+# GOOGLE SPEECH RECOGNITION API:
+
+### source- https://pythonspot.com/speech-recognition-using-google-speech-api/
+
+Google has a great Speech Recognition API which converts spoken text (microphone) into written text (Python strings).
+The audio is recorded using the speech recognition module and the recorded speech is sent to the Google speech recognition API which will then return the output(python string).
+
+(Google Speech API v2 is limited to 50 queries per day and requires a good microphone)
+
+# modules needed- SpeechRecognition, PyAudio
+
+speechrecognition.py program recognises the input audio and then search it in default webbrowser using webbrowser module.
---
+++ speechrecognition.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import webbrowser
+
+r=sr.Recognizer() #by this command we have initialised our recogniser
+
+#If you want to use audio input from microphones, PyAudio is also necessary. If not installed, the
+# library speech_recognition will still work, but Microphone will not be defined
+
+with sr.Microphone() as source: #using with keyword session will be closed automaticallly
+
+    print("Speak!")
+    audio=r.listen(source) #our recogniser will now listen to the source and save it to audio
+
+    try:
+
+        # for testing purposes, we're just using the default API key to use another API key, use
+        # `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")` instead of `r.recognize_google(audio)`
+
+        text=r.recognize_google(audio)   #using this command you can also convert an pre recorded audio to text
+        print("You said : {}".format(text))
+        url='https://www.google.co.in/search?q='
+        search_url=url+text
+        webbrowser.open(search_url)
+        #open the url in your default browser
+
+    except:
+
+        print("Can't recognize")
