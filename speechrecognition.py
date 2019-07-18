97533dfd6a5137172cd579f3f8456f8a0d5c5c6c
harshit muhal <hrshtmuhal8@gmail.com>
2019-07-18 12:01:16 +0530

    change

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,20 +1,20 @@
 import speech_recognition as sr
 import webbrowser
 
-r=sr.Recognizer() #by this command we have initialised our recogniser
+r=sr.Recognizer() # this command initialises the recogniser
 
-#If you want to use audio input from microphones, PyAudio is also necessary. If not installed, the
+# If you want to use audio input from microphones, PyAudio is also necessary. If not installed, the
 # library speech_recognition will still work, but Microphone will not be defined
 
 with sr.Microphone() as source: #using with keyword session will be closed automaticallly
 
     print("Speak!")
-    audio=r.listen(source) #our recogniser will now listen to the source and save it to audio
+    audio=r.listen(source) #the recogniser will now listen to the source and save it to audio
 
     try:
 
-        # for testing purposes, we're just using the default API key to use another API key, use
-        # `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")` instead of `r.recognize_google(audio)`
+        #for testing purposes, we're just using the default API key to use another API key, use
+        #`r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")` instead of `r.recognize_google(audio)`
 
         text=r.recognize_google(audio)   #using this command you can also convert an pre recorded audio to text
         print("You said : {}".format(text))
